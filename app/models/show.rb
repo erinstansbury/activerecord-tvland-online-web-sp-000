911ca112b9a6dfde8dc0b787855ab68e84e43f50
Erin Stansbury <ekstansbury@gmail.com>
2020-07-07 04:09:03 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list
-    self.actors.collect do |a|
+    self.actors.map do |a|
       a.name
     end
   end
