e8f95ab9fee6491164b2cdfa032657739958d54b
Erin Stansbury <ekstansbury@gmail.com>
2020-07-07 03:48:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list
+    show = self.actors
+    show.full_name
+  end
 end
